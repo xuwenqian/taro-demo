dc21f70ec3ccd69be689905050f7391d73d6cd8d
xuwenqian <1289643364@qq.com>
2023-08-24 15:51:35 +0800

    Update index.tsx

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,5 +1,5 @@
-import { View, Text } from '@tarojs/components'
-import { useLoad } from '@tarojs/taro'
+import { View, Text,Canvas } from '@tarojs/components'
+import Taro, { useLoad } from '@tarojs/taro'
 import './index.less'
 
 export default function Index() {
@@ -8,9 +8,20 @@ export default function Index() {
     console.log('Page loaded.')
   })
 
+  const draw1 = ()=>{
+      const context = Taro.createCanvasContext('canvas')
+      context.setStrokeStyle('#00ff00')
+      context.setLineWidth(5)
+      context.rect(0, 0, 100, 100)
+      context.stroke()
+      context.draw()
+
+  }
+
   return (
     <View className='index'>
-      <Text>Hello world!</Text>
+      <Text onClick={draw1}>canvas rect(0, 0, 100, 100)</Text>
+      <Canvas canvasId="canvas" style='width:200px;height:200px'></Canvas>
     </View>
   )
 }
